86433b38847eaa9fa058364b31009bbfa6e4db07
Torillec-dev <matheo.torillec2@gmail.fr>
2025-11-02 13:24:23 +0100

    fin Tutoriel Avancé

--- TutorielAvanc/app/src/main/java/UPsay/ET5/Activity2.java
+++ TutorielAvanc/app/src/main/java/UPsay/ET5/Activity2.java
@@ -11,16 +11,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Activity2 extends AppCompatActivity {
 
     public static final String EXTRA = "message";
     EditText monTexte;
+    Fragment page1 = new FragmentHaut();
+    Fragment page2 = new FragmentBas();
+    FragmentManager fm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_2);
+
+        fm = getSupportFragmentManager();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,5 +44,19 @@ public class Activity2 extends AppCompatActivity {
         messageIntent.putExtra(EXTRA, message);
         setResult(Activity.RESULT_OK, messageIntent);
         finish();
+    }
+
+    public void lancePage1(View view) {
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame, page1);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void lancePage2(View view) {
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame, page2);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
